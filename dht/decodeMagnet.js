aaddfeba7be9b4b7935600db6a40ed09172f9a7c
MTX_TianXia <11602011@qq.com>
2018-03-16 16:53:11 +0800

    bt torrent 种子，大数据分析，可以获得意想不到的收获

---
+++ dht/decodeMagnet.js
@@ -0,0 +1,22 @@
+var a = process.argv.splice(2);
+
+var  magnet_uri = require('magnet-uri');
+var o = magnet_uri.decode(a[0]);
+/*
+console.log(o)
+console.log(o.infoHash)
+*/
+
+var WebTorrent = require('webtorrent');
+var client = new WebTorrent();
+client.add(a[0], function (torrent) 
+{
+	console.log("种子信息解析");
+	console.log(torrent);
+    torrent.files.forEach(function (file) 
+    {
+    	console.log(file);
+    })
+});
+
+// node decodeMagnet.js "magnet:?xt=urn:btih:85F39F1D94917D61277725E7DA85D8177A5C12EB&dn=leaks"
