9796b949c9a625c003adf230b258dc3d241f92d5
MTX_TianXia <11602011@qq.com>
2018-04-20 14:22:30 +0800

    add tomcat CVE-2017-12616 test

--- checkAll.js
+++ checkAll.js
@@ -1,23 +1,25 @@
 var arg = [],//process.argv.splice(2),
-    s = 0 < arg.length ? arg[0] : require('os').homedir() + "/Desktop/untitled\ folder/allurls.txt"// "db/allUrls.txt"  // "/Volumes/MyWork/zfweb.txt"//0< arg.length arg[0]
+    // s1 = 0 < arg.length ? arg[0] : require('os').homedir() + "/Desktop/untitled\ folder/allurls.txt"// "db/allUrls.txt"  // "/Volumes/MyWork/zfweb.txt"//0< arg.length arg[0]
+    s1 = "/Users/xiatian/Desktop/k/tm.txt"// "db/allUrls.txt"  // "/Volumes/MyWork/zfweb.txt"//0< arg.length arg[0]
     , fs = require('fs')
-    , a = fs.readFileSync(s).toString().trim().split("\n"),
+    , a = fs.readFileSync(s1).toString().trim().split("\n"),
     async = require('async'),
     child_process = require('child_process'),
     md5 = require('md5'),
     kkk = require('./lib/core_new.js');
 
+var g_oUrl = {};
 async.mapLimit(a, 2,function(s,fnCbk1)
 {
 	
 	var sFn = "./data/" + md5(s) + ".txt";
-	
 	var r = new kkk(s);
 	var sFn = r.rstPath + '/' + r.md5(s) + ".txt";
 	if(fs.existsSync(sFn))
 	{
 		console.log("已经执行过了，跳过：" + s);
-		fnCbk1();
+		if(!g_oUrl[s])g_oUrl[s] = 1,fnCbk1();
+		return;
 	}
 	r.on('info',function(s)
 	{
@@ -26,7 +28,7 @@ async.mapLimit(a, 2,function(s,fnCbk1)
 	r.on('over',function()
 	{
 		console.log(s);
-		fnCbk1();
+		if(!g_oUrl[s])g_oUrl[s] = 1,fnCbk1();
 	});
 	
 	r.on('log',function(s)
