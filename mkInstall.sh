0b89c5d50ac5430b0509baedef269f8a426083dc
MTX_TianXia <11602011@qq.com>
2018-04-26 10:53:42 +0800

    update install.sh

--- mkInstall.sh
+++ mkInstall.sh
@@ -2,8 +2,12 @@ rm ins.tmp
 find . -name "*.js" -exec grep -h -R  -Eo "require\(['\"]([^\.][^'\"\/]+?)['\"]\)" {} \; >>ins.tmp
 grep -Eo  "['\"]([^'\"\.\/]+?)['\"]" ins.tmp |sed 's/"//g'|sed "s/'//g"|sort|uniq >>ins1.tmp
 rm ins.tmp
-cat ins1.tmp | awk '{print "npm i -g "$1}' > install.sh
+ls -1 /usr/local/lib/node_modules/ >ins3.tmp
+grep -F -f ins3.tmp ins1.tmp| sort | uniq > ins4.tmp
+cat ins4.tmp | awk '{print "npm i -g "$1}' > install.sh
 rm ins1.tmp
+rm ins4.tmp
+rm ins3.tmp
 chmod 555 install.sh
 git add install.sh
 git commit -m "update install.sh" .
