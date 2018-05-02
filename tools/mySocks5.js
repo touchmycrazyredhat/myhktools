0ebca6ef1a4791612a069aed1fc8915eaf9a7131
MTX_TianXia <11602011@qq.com>
2018-05-02 15:41:18 +0800

    修正sockes代理

--- tools/mySocks5.js
+++ tools/mySocks5.js
@@ -2,6 +2,12 @@
 node tools/mySocks5.js --user mtxuser --password Wr90,_x*d -p 15533
 node proxy/ProxyServer.js --proxy 'socks://mtxuser:Wr90,_x*d@127.0.0.1:15533'
 curl -x "http://127.0.0.1:8880" http://ip.cn
+
+curl -x "socks5://mtxuser:Wr90,_x*d@127.0.0.1:15533" http://ip.cn
+curl -x "socks5://127.0.0.1:1089" http://ip.cn
+
+同时使用多个vps线路的情况，苹果系统代理不能设置密码，否则各种问题
+proxychains4 -f ~/safe/`whoami`/proxychains.conf node /Users/`whoami`/safe/myhktools/tools/mySocks5.js -p 15533
 */
 var socks = require('socksv5'),
 	program = require('commander'),
@@ -20,8 +26,8 @@ var socks = require('socksv5'),
 program.version("socks5代理")
 	.option('-p, --port [value]', '端口,默认 15533')
 	.option('-h, --host [value]', '绑定的ip，默认0.0.0.0')
-	.option('-u, --user [value]', '用户名, 默认：mtx')
-	.option('-p, --password [value]', "密码, 默认：`!';/.;l'Qgdf097")
+	.option('-u, --user [value]', '用户名')
+	.option('-p, --password [value]', "密码")
 	.parse(process.argv);
 process.on('uncaughtException', function(e){});
 process.on('unhandledRejection', function(e){});
@@ -31,6 +37,8 @@ srv.listen(program.port||15533, program.host||'0.0.0.0', function()
 	console.log('SOCKS server listening...' + this._connectionKey);
 });
 
-srv.useAuth(socks.auth.UserPassword(function(user, password, cb) {
-  cb(user === (program.user || 'mtx') && password === (program.password || "`!';/.;l'Qgdf097"));
-}));
+if(program.user && program.password)
+	srv.useAuth(socks.auth.UserPassword(function(user, password, cb) {
+	  cb(user === (program.user || 'mtx') && password === (program.password || "`!';/.;l'Qgdf097"));
+	}));
+else srv.useAuth(socks.auth.None());
