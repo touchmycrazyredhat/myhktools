b4dce7ffcf8f2ba181effee13ac62d610d41a4fc
hktalent <3135773@qq.com>
2018-10-17 03:55:29 -0600

    add tools/expOracle.py

---
+++ tools/expOracle.py
@@ -0,0 +1,44 @@
+#!env python
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+import os
+import cx_Oracle
+
+# os.environ["NLS_LANG"] = "RUSSIAN_RUSSIA.AL32UTF8"
+
+# py2 /root/mytools/myhktools/tools/expOracle.py xx/xx@12.18.102.5/xxx
+connection = sys.argv[1]
+orcl = cx_Oracle.connect(connection,encoding = "UTF-8", nencoding = "UTF-8")
+curs = orcl.cursor()
+
+printHeader = True # include column headers in each table output
+
+sql = "select owner,TABLE_NAME,NUM_ROWS from all_tables where not 'CHANGE_ON_INSTALL,CTXSYS,DBSNMP,INTERNAL,LBACSYS,MANAGER,MDSYS,MTRPW,MTSSYS,ODM,ODM_MTR,OLAPSYS,ORACLE,ORDPLUGINS,ORDSYS,OUTLN,SCOTT,SYS,SYSTEM,TIGER' like '%'||owner||'%'  and num_rows>1000 and not (table_name like '%LOG%' or  table_name like '%$%' or table_name like '%BAK%' or table_name like '%TMP%' or table_name like '%TEMP%' or table_name like '%TEST%') order by num_rows desc"
+curs.execute(sql)
+
+for row_data in curs:
+    if not row_data[1].startswith('BIN$'): # skip recycle bin tables
+        tableName = row_data[1]
+        dbName = row_data[0]
+
+        # output each table content to a separate CSV file
+        csv_file_dest = dbName + "." + tableName + ".csv"
+        print csv_file_dest
+        outputFile = open(csv_file_dest,'w') # 'wb'
+        output = csv.writer(outputFile, dialect='excel')
+        sql = "select * from " + dbName + "." + tableName
+        curs2 = orcl.cursor()
+        curs2.execute(sql)
+
+        if printHeader: # add column headers if requested
+            cols = []
+            for col in curs2.description:
+                cols.append(col[0])
+            output.writerow(cols)
+
+        for row_data in curs2: # add table rows
+            output.writerow(row_data)
+
+        outputFile.close()
