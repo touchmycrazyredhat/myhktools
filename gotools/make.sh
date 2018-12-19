5a46c4b017cf14822277e4a1505646b0871392dd
hktalent <3135773@qq.com>
2018-12-19 23:39:46 +0800

    add go hacker tools

---
+++ gotools/make.sh
@@ -0,0 +1,5 @@
+go get -v -u github.com/google/gopacket
+go get -v -u github.com/google/gopacket/layers
+go get -v -u github.com/google/gopacket/pcap
+# GOOS=windows GOARCH=386 go build -o gosocks.exe socks5.go
+
