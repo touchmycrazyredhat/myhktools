92d9acd1e4a15c719a5b3f3c7c7abae80faee371
hktalent <3135773@qq.com>
2018-12-20 17:03:59 +0800

    del

--- gotools/make.sh
+++
@@ -1,5 +0,0 @@
-go get -v -u github.com/google/gopacket
-go get -v -u github.com/google/gopacket/layers
-go get -v -u github.com/google/gopacket/pcap
-# GOOS=windows GOARCH=386 go build -o gosocks.exe socks5.go
-
