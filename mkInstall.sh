e1c7ddc0a5982783af2c16e1f4206db58187fc5b
hktalent <3135773@qq.com>
2019-04-23 16:58:10 +0800

    fix

--- mkInstall.sh
+++ mkInstall.sh
@@ -1,4 +1,6 @@
-rm ins.tmp
+if [[ -f "ins.tmp" ]];then
+    rm ins.tmp
+fi
 find . -name "*.js" -exec grep -h -R  -Eo "require\(['\"]([^\.][^'\"\/]+?)['\"]\)" {} \; >>ins.tmp
 grep -Eo  "['\"]([^'\"\.\/]+?)['\"]" ins.tmp |sed 's/"//g'|sed "s/'//g"|sort|uniq >>ins1.tmp
 if [[ -f "ins.tmp" ]];then
@@ -7,6 +9,9 @@ fi
 xx="/root/mytools/node-v10.12.0-linux-x64/lib/node_modules/"
 if ![[ -d $xx ]];then
     xx="/usr/local/lib/node_modules/"
+fi
+if ![[ -d $xx ]];then
+    xx="/root/local/lib/node_modules"
 fi
 ls -1 $xx >ins3.tmp
 grep -F -f ins3.tmp ins1.tmp| sort | uniq > ins4.tmp
